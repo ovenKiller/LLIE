4873184445c780520f6320d46d3eb2782beaf143
ovenkiller <light7777777@foxmail.com>
2023-04-29 13:17:25 +0800

    bug-fix

--- feature_test/count_flops.py
+++ feature_test/count_flops.py
@@ -1,10 +1,6 @@
-from ptflops import get_model_complexity_info
-import model
+import ptflops
 def count_flops(model):
-    macs, params = get_model_complexity_info(model, (3, 256, 256), as_strings=True,
+    macs, params = ptflops.get_model_complexity_info(model, (3, 256, 256), as_strings=True,
                                          print_per_layer_stat=True, verbose=True)
     print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
     print('{:<30}  {:<8}'.format('Number of parameters: ', params))
-
-model = model.less_conv_dce()
-count_flops(model)
